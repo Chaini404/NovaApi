e326541da605c7faa7bd072473d75326f0afb9b9
Angely <eusebioangely@gmail.com>
2025-12-20 21:04:39 -0500

    cambios

--- src/main/java/com/cibertec/ApiNova/emergencyEvent/dtos/response/EmergencyEventResponse.java
+++ src/main/java/com/cibertec/ApiNova/emergencyEvent/dtos/response/EmergencyEventResponse.java
@@ -8,6 +8,7 @@ import com.cibertec.ApiNova.emergencyEvent.model.type.EmergencyEventStatus;
 public record EmergencyEventResponse(
         Long id,
         Long userId,
+
         EmergencyEventStatus status,
         LocalDateTime activatedAt,
         LocalDateTime closedAt
